9dd7b6561b430b1e3629ae792437ed82d0c58ed8
half144 <guilhermefeller2002@gmail.com>
2023-03-14 15:16:36 -0300

    fix-envoriment

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const apiKey = process.env.API_KEY;
-
 export const api = axios.create({
   baseURL: "https://api.openai.com/v1",
 });
 
-api.defaults.headers.common["Authorization"] = `Bearer ${apiKey}`;
+api.defaults.headers.common[
+  "Authorization"
+] = `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`;
